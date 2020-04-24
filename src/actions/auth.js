cbdf1c76c992c9d5cb367e9ab61e69fdcf861b04
Charan Tej Kandukuri <kcharantej@outlook.com>
2020-04-24 23:59:01 +0530

    Logout Navbar Links

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -7,6 +7,7 @@ import {
   AUTH_ERROR,
   LOGIN_SUCCESS,
   LOGIN_FAIL,
+  LOGOUT,
 } from "./types";
 import setAuthToken from "../utils/setAuthToken";
 
@@ -80,4 +81,10 @@ export const login = (email, password) => async (dispatch) => {
       type: LOGIN_FAIL,
     });
   }
+};
+
+export const logout = () => (dispatch) => {
+  dispatch({
+    type: LOGOUT,
+  });
 };
